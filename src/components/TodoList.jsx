155eb9558b50099ec9ce8ed5dd11d8d86f2ef177
CloneTrooper66 <luka.eli88@gmail.com>
2024-07-22 13:35:38 +0100

    displaying todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import TodoCard from "./TodoCard";
 
 export default function TodoList() {
-  return <div>TodoList</div>;
+  let todos = [
+    "Go to the gym",
+    "Eat more fruits",
+    "Pick up the kids from school",
+  ];
+  return (
+    <ul className="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard key={todoIndex}>
+            <p>{todo}</p>
+          </TodoCard>
+        );
+      })}
+    </ul>
+  );
 }
